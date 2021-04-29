5b6dd45a69d9df2e2ed332926b68279556b4ead2
Gergely Kerekes <kerekes.gergely83@gmail.com>
2021-04-29 10:50:04 +0200

    project finished

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -34,27 +34,10 @@ public class JurassicPark {
                 dinos.add(dino);
             }
         } catch (SQLException e) {
-            System.out.println("File not found!");;
+            System.out.println("File not found!");
+            ;
         }
         return dinos;
     }
 
 }
-
-/*
-Az adatbázis egyetlen táblát tartalmaz, dinosaur néven, az alábbi oszlopokkal:
-
-breed varchar (elsődleges kulcs)
-expected int
-actual int
- */
-/*
-Készítsd el a checkOverpopulation metódust! A checkOverpopulation metódus feladata,
-hogy térjen vissza azoknak a fajtáknak a nevével, amiknél a valós létszám magasabb,
-mint az elvárt (a fenti példában a Maiasaurus, a Velociraptor és a Hypsilophodontida),
-ABC szerint növekvő sorrendben.
-
-Ha az adatbázis üres, akkor a metódus térjen vissza üres listával.
-
-A megoldás során használj PreparedStatement-et!
- */
